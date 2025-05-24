e255e7b26a54361e9dcab7b234c36dd32597a1c7
Abhimanyu <methebestestofall@gmail.com>
2025-05-24 14:35:32 -0400

    tried to add basic code

---
+++ assets/scripts/coin.js
@@ -0,0 +1,18 @@
+function getRandomZeroOrOne() {
+  return Math.round(Math.random());
+}
+
+function coinflip() {
+    const result = document.getElementById('result');
+
+    let randomNumber = getRandomZeroOrOne();
+
+    if (randomNumber = 1) {
+        result.innerHTML = "<h1>Heads</h1>"
+    } if (randomNumber = 0) {
+        result.innerHTML = "<h1>Tails</h1>"
+    } else {
+        result.innerHTML = "error"
+        alert("error getting coin result")
+    }
+}
