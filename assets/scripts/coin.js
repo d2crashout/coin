ca6b808f397afabfb4080fee9a07766f94e476d8
Abhimanyu <methebestestofall@gmail.com>
2025-05-25 10:22:56 -0400

    fixed function name

--- assets/scripts/coin.js
+++ assets/scripts/coin.js
@@ -2,14 +2,14 @@ function getRandomZeroOrOne() {
   return Math.round(Math.random());
 }
 
-function coinflip() {
+function coinFlip() {
     const result = document.getElementById('result');
 
     let randomNumber = getRandomZeroOrOne();
 
-    if (randomNumber = 1) {
+    if (randomNumber === 1) {
         result.innerHTML = "<h1>Heads</h1>"
-    } if (randomNumber = 0) {
+    } if (randomNumber === 0) {
         result.innerHTML = "<h1>Tails</h1>"
     } else {
         result.innerHTML = "error"
